b171a3bfb5280f0c6b3c98e2b2da08aa929f0e8d
preetinadkarni <61760445+preetinadkarni@users.noreply.github.com>
2020-05-12 22:11:25 +1200

    Income Tax calculated

--- BasicPayslip.cs
+++ BasicPayslip.cs
@@ -11,6 +11,8 @@ public class BasicPayslip
 
 	private double grossIncome;
 
+	private int incomeTax;
+
 	public BasicPayslip()
 	{
 	}
@@ -20,6 +22,22 @@ public class BasicPayslip
 		grossIncome = Math.Floor(Convert.ToDouble(AnnualSalary) / 12);
 		return grossIncome;
 	}
+
+	public int CalculateIncomeTax()
+	{
+		double tax = 0;
+
+		if ( AnnualSalary >= 18201 && AnnualSalary <= 37000 )
+			tax = ( (AnnualSalary - 18200) * 0.19 ) / 12;
+		else if ( AnnualSalary >= 37001 && AnnualSalary <= 87000 )
+			tax = ( 3572 + ((AnnualSalary - 37000) * 0.325 ))/ 12;
+		else if ( AnnualSalary >= 87001 && AnnualSalary <= 180000)
+			tax = ( 19822 + ((AnnualSalary - 87000) * 0.37)) / 12;
+		else if ( AnnualSalary >= 180000)
+			tax = ( 54232 + ((AnnualSalary - 180000) * 0.45)) / 12;
+
+		return (Int32)Math.Ceiling(tax);
+	}
 	public void GeneratePayslip()
 	{
 		Console.WriteLine("\nYour payslip has been generated:\n");
@@ -27,6 +45,8 @@ public class BasicPayslip
 		Console.WriteLine($"Pay Period: {StartDate} - {EndDate}");
 		this.CalculateGrossIncome();
 		Console.WriteLine($"Gross Income: {grossIncome}");
+		incomeTax = CalculateIncomeTax();
+		Console.WriteLine($"Income Tax: {incomeTax}");
 		Console.ReadKey();
 	}
 
--- BasicPayslipUnitTests.cs
+++ BasicPayslipUnitTests.cs
@@ -6,18 +6,26 @@ namespace BasicPayslipUnitTests
     [TestFixture]
     public class BasicPayslipUnitTests
     {
-        //[TestCase(0, ExpectedResult = true)]
-        //public bool GivenSuperRateIsNumber_WhenCheckSuperRateIsValidIsCalled_ThenItShouldCheckIfItIsBetween0And50(int superRate)
-        //{
-        //    return CheckSuperRateIsValid(superRate);
-        //}
-
         [Test]
         public void GivenAnnualSalary_WhenCalculateGrossSalary_ThenCorrectGrossSalaryCalculated()
         {
             BasicPayslip bp = new BasicPayslip();
             bp.AnnualSalary = 60050;
             Assert.AreEqual(bp.CalculateGrossIncome(), 5004);
+        }
+
+        [TestCase(17000, ExpectedResult = 0)]
+        [TestCase(18201, ExpectedResult = 1)]
+        [TestCase(38000, ExpectedResult = 325)]
+        [TestCase(60050, ExpectedResult = 922)]
+        [TestCase(87000, ExpectedResult = 1652)]
+        [TestCase(90000, ExpectedResult = 1745)]
+        [TestCase(180001, ExpectedResult = 4520)]
+        public int GivenAnnualSalary_WhenCalculateIncomeTax_ThenCorrectIncomeTaxCalculated(int annualSalary)
+        {
+            BasicPayslip bp = new BasicPayslip();
+            bp.AnnualSalary = annualSalary;
+            return bp.CalculateIncomeTax();
         }
     }
 }

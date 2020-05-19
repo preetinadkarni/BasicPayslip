26873acc4725822244ecc0b7f8da74e62e640807
preetinadkarni <61760445+preetinadkarni@users.noreply.github.com>
2020-05-19 16:07:38 +1200

    changed double to decimal
    added const totalMonths.
    changed double to decimal to calculate to accurate precision. 
    the variables are kept as integers based on the kata requirements.

--- BasicPayslip.cs
+++ BasicPayslip.cs
@@ -15,36 +15,37 @@ public class BasicPayslip
 
 	public int CalculateGrossIncome()
 	{
-		double grossIncome;
-		grossIncome = Math.Floor(Convert.ToDouble(AnnualSalary) / 12);
+		decimal grossIncome;
+		grossIncome = Math.Floor(Convert.ToDecimal(AnnualSalary) / 12);
 		return (Int32)grossIncome;
 	}
 
 	public int CalculateIncomeTax()
 	{
-		double tax = 0;
+		decimal tax = 0;
 
 		const int TaxSlab1Max = 18200;
 		const int TaxSlab2Min = 18201;
 		const int TaxSlab2Max = 37000;
-		const double Slab2Tax = 0.19;
+		const decimal Slab2Tax = 0.19M;
 		const int TaxSlab3Min = 37001;
 		const int TaxSlab3Max = 87000;
-		const double Slab3Tax = 0.325;
+		const decimal Slab3Tax = 0.325M;
 		const int TaxSlab4Min = 87001;
 		const int TaxSlab4Max = 180000;
-		const double Slab4Tax = 0.37;
+		const decimal Slab4Tax = 0.37M;
 		const int TaxSlab5Min = 180001;
-		const double Slab5Tax = 0.45;
+		const decimal Slab5Tax = 0.45M;
+		const int totalMonths = 12;
 
 		if ( AnnualSalary >= TaxSlab2Min && AnnualSalary <= TaxSlab2Max)
-			tax = ( (AnnualSalary - TaxSlab1Max) *  Slab2Tax) / 12;
+			tax = ( (AnnualSalary - TaxSlab1Max) *  Slab2Tax) / totalMonths;
 		else if ( AnnualSalary >= TaxSlab3Min && AnnualSalary <= TaxSlab3Max)
-			tax = ( 3572 + ((AnnualSalary - TaxSlab2Max) * Slab3Tax ))/ 12;
+			tax = ( 3572 + ((AnnualSalary - TaxSlab2Max) * Slab3Tax ))/ totalMonths;
 		else if ( AnnualSalary >= TaxSlab4Min && AnnualSalary <= TaxSlab4Max)
-			tax = ( 19822 + ((AnnualSalary - TaxSlab3Max) * Slab4Tax)) / 12;
+			tax = ( 19822 + ((AnnualSalary - TaxSlab3Max) * Slab4Tax)) / totalMonths;
 		else if ( AnnualSalary >= TaxSlab5Min)
-			tax = ( 54232 + ((AnnualSalary - TaxSlab4Max) * Slab5Tax)) / 12;
+			tax = ( 54232 + ((AnnualSalary - TaxSlab4Max) * Slab5Tax)) / totalMonths;
 
 		return (Int32)Math.Ceiling(tax);
 	}
@@ -57,7 +58,7 @@ public class BasicPayslip
 
 	public int CalculateSuper()
 	{
-		double super = Math.Floor(Convert.ToDouble(CalculateGrossIncome() * SuperRate / 100));
+		decimal super = Math.Floor(Convert.ToDecimal(CalculateGrossIncome() * SuperRate / 100));
 		return (Int32)super;
 	}
 	public void GeneratePayslip()

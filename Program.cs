4fbe65e4581c25c32c1f23600dd5b5fd01702669
preetinadkarni <61760445+preetinadkarni@users.noreply.github.com>
2020-06-04 16:40:01 +1200

    Attempt to make the input parameters function smaller

--- Program.cs
+++ Program.cs
@@ -7,100 +7,67 @@ using System.Threading.Tasks;
 namespace BasicPayslipKata
 {
     public class Program
-    {     
+    {
         public static void Main()
         {
             BasicPayslip bp = new BasicPayslip();
-            InputPayslipDetails(bp);
+            GetPayslipDetails(bp);
             bp.GeneratePayslip();
 
         }
-        static void InputPayslipDetails(BasicPayslip bp)
+        private static void GetPayslipDetails(BasicPayslip bp)
         {
-            Console.WriteLine("~~~");
-            Console.WriteLine("Welcome to the payslip generator!\n");
-            bool isValidName = false;
-            while (!isValidName)
+            Console.WriteLine("~~~\nWelcome to the payslip generator!\n");
+            bool[] isValidInput = { false, false, false, false, false, false };
+            string[] inputMessages = { "name", "surname", "annual salary", "super rate", "payment start date", "payment end date" };
+            string[] inputItems = new string[6];
+            int i = 0;
+            while (i < inputMessages.Length)
             {
-                Console.Write("Please input your name: ");
-                string name = Console.ReadLine();
-                if (!string.IsNullOrEmpty(name))
+                while (!isValidInput[i])
                 {
-                    isValidName = true;
-                    bp.Name = name;
+                    Console.Write($"Please input your {inputMessages[i]}: ");
+                    string consoleInput = Console.ReadLine();
+                    if (ValidateInput(inputMessages[i], consoleInput))
+                    {
+                        isValidInput[i] = true;
+                        inputItems[i] = consoleInput;
+                    }                  
                 }
+                i++;
             }
+            bp.Name = inputItems[0];
+            bp.Surname = inputItems[1];
+            bp.AnnualSalary = Convert.ToInt32(inputItems[2]);
+            bp.SuperRate = Convert.ToInt32(inputItems[3]);
+            bp.StartDate = inputItems[4];
+            bp.EndDate = inputItems[5];
 
-            bool isValidSurname = false;
-            while (!isValidSurname)
+        }
+        private static bool ValidateInput(string inputMessage, string consoleInput)
+        {
+            bool success;
+            switch (inputMessage)
             {
-                Console.Write("Please input your surname: ");
-                string surname = Console.ReadLine();
-                if (!string.IsNullOrEmpty(surname))
-                {
-                    isValidSurname = true;
-                    bp.Surname = surname;
-                }
-            }
-
-            bool isValidAnnualSalary = false;
-            while (!isValidAnnualSalary)
-            {
-                Console.Write("Please enter your annual salary: ");
-                string ansal = Console.ReadLine();
-                int annualSalary;
-                bool success = Int32.TryParse(ansal, out annualSalary);
-                const int positiveAnnualSalary = 0;
-                if (success && (annualSalary > positiveAnnualSalary))
-                {
-                    isValidAnnualSalary = true;
-                    bp.AnnualSalary = annualSalary;
-
-                }
-                else
-                    Console.WriteLine("Annual Salary should be positive integer");
-
-            }
-
-            bool isValidSuperRate = false;
-            while (!isValidSuperRate)
-            {
-                Console.Write("Please enter your super rate: ");
-                string sr = Console.ReadLine();
-                int superRate;
-                bool success = Int32.TryParse(sr, out superRate);
-                if (success && CheckSuperRateIsValid(superRate))
-                {
-                    isValidSuperRate = true;
-                    bp.SuperRate = superRate;
-                }
-                else
-                    Console.WriteLine("Super Rate must be between 0% and 50% inclusive");
-
-            }
-
-            bool isValidStartDate = false;
-            while (!isValidStartDate)
-            {
-                Console.Write("Please enter your payment start date: ");
-                string startDate = Console.ReadLine();
-                if (!string.IsNullOrEmpty(startDate))
-                {
-                    isValidStartDate = true;
-                    bp.StartDate = startDate;
-                }
-            }
-
-            bool isValidEndDate = false;
-            while (!isValidEndDate)
-            {
-                Console.Write("Please enter your payment end date: ");
-                string endDate = Console.ReadLine();
-                if (!string.IsNullOrEmpty(endDate))
-                {
-                    isValidEndDate = true;
-                    bp.EndDate = endDate;
-                }
+                case "annual salary":
+                    int annualSalary;
+                    success = Int32.TryParse(consoleInput, out annualSalary);
+                    const int positiveAnnualSalary = 0;
+                    if (success && (annualSalary > positiveAnnualSalary))
+                       return true;
+                    else
+                        Console.WriteLine("Annual Salary should be positive integer");
+                    return false;
+                case "super rate":
+                    int superRate;
+                    success = Int32.TryParse(consoleInput, out superRate);
+                    if (success && CheckSuperRateIsValid(superRate))
+                        return true;
+                    else
+                        Console.WriteLine("Super Rate must be between 0% and 50% inclusive");
+                    return false;
+                default:
+                    return !string.IsNullOrEmpty(consoleInput);
             }
         }
         public static bool CheckSuperRateIsValid(int superRate)

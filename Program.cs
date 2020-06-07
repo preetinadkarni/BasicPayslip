7ed36c4c0242fa3ceea1a5c41736bde89972d3c6
preetinadkarni <61760445+preetinadkarni@users.noreply.github.com>
2020-06-08 09:13:06 +1200

    Modified to make it smaller and precise functions

--- Program.cs
+++ Program.cs
@@ -11,64 +11,112 @@ namespace BasicPayslipKata
         public static void Main()
         {
             BasicPayslip bp = new BasicPayslip();
-            GetPayslipDetails(bp);
+            ShowWelcome();
+            bp.Name = GetName();
+            bp.Surname = GetSurname();
+            bp.AnnualSalary = GetAnnualSalary();
+            bp.SuperRate = GetSuperRate();
+            bp.StartDate = GetStartDate();
+            bp.EndDate = GetEndDate();
             bp.GeneratePayslip();
-
         }
-        private static void GetPayslipDetails(BasicPayslip bp)
-        {
-            Console.WriteLine("~~~\nWelcome to the payslip generator!\n");
-            bool[] isValidInput = { false, false, false, false, false, false };
-            string[] inputMessages = { "name", "surname", "annual salary", "super rate", "payment start date", "payment end date" };
-            string[] inputItems = new string[6];
-            int i = 0;
-            while (i < inputMessages.Length)
-            {
-                while (!isValidInput[i])
-                {
-                    Console.Write($"Please input your {inputMessages[i]}: ");
-                    string consoleInput = Console.ReadLine();
-                    if (ValidateInput(inputMessages[i], consoleInput))
-                    {
-                        isValidInput[i] = true;
-                        inputItems[i] = consoleInput;
-                    }                  
-                }
-                i++;
-            }
-            bp.Name = inputItems[0];
-            bp.Surname = inputItems[1];
-            bp.AnnualSalary = Convert.ToInt32(inputItems[2]);
-            bp.SuperRate = Convert.ToInt32(inputItems[3]);
-            bp.StartDate = inputItems[4];
-            bp.EndDate = inputItems[5];
 
-        }
-        private static bool ValidateInput(string inputMessage, string consoleInput)
+        public static void ShowWelcome()
         {
-            bool success;
-            switch (inputMessage)
+            Console.WriteLine("~~~");
+            Console.WriteLine("Welcome to the payslip generator!\n");
+        }
+
+        public static string GetName()
+        {
+            bool isValidName = false;
+            string name;
+            do
             {
-                case "annual salary":
-                    int annualSalary;
-                    success = Int32.TryParse(consoleInput, out annualSalary);
-                    const int positiveAnnualSalary = 0;
-                    if (success && (annualSalary > positiveAnnualSalary))
-                       return true;
-                    else
-                        Console.WriteLine("Annual Salary should be positive integer");
-                    return false;
-                case "super rate":
-                    int superRate;
-                    success = Int32.TryParse(consoleInput, out superRate);
-                    if (success && CheckSuperRateIsValid(superRate))
-                        return true;
-                    else
-                        Console.WriteLine("Super Rate must be between 0% and 50% inclusive");
-                    return false;
-                default:
-                    return !string.IsNullOrEmpty(consoleInput);
-            }
+                Console.Write("Please input your name: ");
+                name = Console.ReadLine();
+                if (!string.IsNullOrEmpty(name))
+                    isValidName = true;
+            } while (!isValidName);
+            return name;                  
+        }
+
+        public static  string GetSurname()
+        {
+            bool isValidSurname = false;
+            string surname;
+            do
+            {
+                Console.Write("Please input your surname: ");
+                surname = Console.ReadLine();
+                if (!string.IsNullOrEmpty(surname))
+                    isValidSurname = true;
+            } while (!isValidSurname);
+            return surname;
+        }
+
+        public static int GetAnnualSalary()
+        {
+           bool isValidAnnualSalary = false;
+           int annualSalary;
+           do
+            {
+                Console.Write("Please enter your annual salary: ");
+                string ansal = Console.ReadLine();
+                bool success = Int32.TryParse(ansal, out annualSalary);
+                const int positiveAnnualSalary = 0;
+                if (success && (annualSalary > positiveAnnualSalary))
+
+                    isValidAnnualSalary = true;
+                else
+                    Console.WriteLine("Annual Salary should be positive integer");
+            } while (!isValidAnnualSalary) ;
+            return annualSalary;
+        }
+
+        public static int GetSuperRate()
+        {
+            bool isValidSuperRate = false;
+            int superRate;
+            do
+            {
+                Console.Write("Please enter your super rate: ");
+                string sr = Console.ReadLine();
+                bool success = Int32.TryParse(sr, out superRate);
+                if (success && CheckSuperRateIsValid(superRate))
+                    isValidSuperRate = true;
+                else
+                    Console.WriteLine("Super Rate must be between 0% and 50% inclusive");
+
+            } while (!isValidSuperRate);
+            return superRate;
+        }
+
+        public static string GetStartDate()
+        {
+            bool isValidStartDate = false;
+            string startDate;
+            do
+            {
+                Console.Write("Please enter your payment start date: ");
+                startDate = Console.ReadLine();
+                if (!string.IsNullOrEmpty(startDate))
+                    isValidStartDate = true;
+            } while (!isValidStartDate);
+            return startDate;
+        }
+        public static string GetEndDate()
+        {
+            bool isValidEndDate = false;
+            string endDate;
+            do
+            {
+                Console.Write("Please enter your payment end date: ");
+                endDate = Console.ReadLine();
+                if (!string.IsNullOrEmpty(endDate))
+                    isValidEndDate = true;
+            } while (!isValidEndDate) ;
+            return endDate;
         }
         public static bool CheckSuperRateIsValid(int superRate)
         {
